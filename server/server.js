b9ebd0f718e33ec3106a11b79c3de7c4c102b4f8
viratKohli18Us <karthikohli2119@gmail.com>
2025-07-03 13:36:28 +0530

    Added Vercel URL to CORS config

--- server/server.js
+++ server/server.js
@@ -15,7 +15,17 @@ const allowedOrigins = ['http://localhost:5173','https://mern-auth-backend-g2qd.
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:allowedOrigins, credentials:true}))
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 
 /// api routes -----------
 app.use('/api/auth',authRouter)   ///  url ==> localhost:4000/api/auth/[register,login,logout]
